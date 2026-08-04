dd812a855c1ea3a378ea851d29f58931b9fb23ee
gen740 <54583542+gen740@users.noreply.github.com>
2026-08-04 16:10:54 +0900

    feat: expose event counter in raw output

--- flake.nix
+++ flake.nix
@@ -8,7 +8,12 @@
   };
 
   outputs =
-    inputs@{ self, flake-parts, nixpkgs, ... }:
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.platforms.all;
 
@@ -16,7 +21,14 @@
         { pkgs, ... }:
         let
           baseBuildInputs =
-            (with pkgs; [ cmake ninja automake autoconf root python3 ])
+            (with pkgs; [
+              cmake
+              ninja
+              automake
+              autoconf
+              root
+              python3
+            ])
             ++ pkgs.lib.optional pkgs.stdenv.hostPlatform.isGnu pkgs.glibc.static;
           staticHook = pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isGnu ''
             export LIBRARY_PATH="${pkgs.glibc.static}/lib"''${LIBRARY_PATH:+:$LIBRARY_PATH}
@@ -24,7 +36,10 @@
         in
         {
           devShells.default = pkgs.mkShell {
-            packages = baseBuildInputs ++ [ pkgs.cmake-format ];
+            packages = baseBuildInputs ++ [
+              pkgs.cmake-format
+              pkgs.python3
+            ];
             shellHook = ''
               unset NIX_LDFLAGS
               unset NIX_CFLAGS_LINK
@@ -35,6 +50,7 @@
           packages.default = pkgs.stdenv.mkDerivation {
             name = "hero_shell";
             src = self;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
             buildInputs = baseBuildInputs;
             preConfigure = ''
               unset NIX_LDFLAGS
@@ -50,6 +66,10 @@
               "-DCMAKE_BUILD_TYPE=Release"
               "-DCMAKE_IGNORE_PREFIX_PATH=/nix/store"
             ];
+            postFixup = ''
+              wrapProgram "$out/bin/hero_shell" \
+                --prefix PATH : "$out/bin:${pkgs.lib.makeBinPath [ pkgs.python3 ]}"
+            '';
           };
         };
     };
